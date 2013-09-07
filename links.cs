f87c897c36b44caeff022bca011f4283ed8a815c
Egor Chelak <egor.chelak@gmail.com>
2013-09-08 01:03:14 +0300

    No more outdated server error. Made links use parent form thing.

--- links.cs
+++ links.cs
@@ -16,5 +16,18 @@ namespace CCBin
         {
             InitializeComponent();
         }
+        
+        public main parent = null;
+        public links setParent(main par, string str, int w, int h)
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(links));
+            this.parent = par;
+            richTextBox1.Text = resources.GetString(str);
+            this.Size = new System.Drawing.Size(w, h);
+            this.Text = str;
+            this.MaximumSize = new System.Drawing.Size(w, h);
+            this.MinimumSize = new System.Drawing.Size(w, h);
+            return this;
+        }
     }
 }
